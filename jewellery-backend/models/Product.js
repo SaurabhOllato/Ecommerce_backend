bdb27b4f13c7b4744f400fd56ab9d07995d458f8
SaurabhOllato <saurabh.ollato@gmail.com>
2025-06-21 16:48:37 +0530

    modified product.js

--- jewellery-backend/models/Product.js
+++ jewellery-backend/models/Product.js
@@ -1,17 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    price: {
+      type: Number,
+      required: true,
+    },
+    inStock: { type: Boolean, default: true },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    category: String, // optional (ring, necklace, etc.)
   },
-  description: String,
-  price: Number,
-  imageUrl: {
-    type: String,
-    required: true,
-  },
-  category: String, // optional (ring, necklace, etc.)
-}, { timestamps: true });
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model("Product", productSchema);
